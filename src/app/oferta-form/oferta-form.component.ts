833a6fd7cc838f1b5e225286439067c2ab3ff5a9
hector <hector.ninian@gmail.com>
2022-07-25 12:26:59 +0200

    avance formulario creacióm manual de oferta

--- src/app/oferta-form/oferta-form.component.ts
+++ src/app/oferta-form/oferta-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-oferta-form',
@@ -7,9 +9,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfertaFormComponent implements OnInit {
 
-  constructor() { }
+  tipo : string;
+  denomincion : string; //O
+  convocante : string; //Obligatorio
+  url: string;
+  f_inicioPresentacion: Date; //O
+  f_finPresentacion: Date; //0
+  contacto: string;
+  titulo: string;
+  plazas: number;
+
+  showSuccess = false;
+  showFFinError = false;
+  showFInitError = false;
+  showConvError = false;
+  showDenomError = false;
+
+
+
+
+  constructor( private http: HttpClient, private router: Router ) { }
 
   ngOnInit(): void {
+    this.tipo = '';
+    this.denomincion = '';
+    this.convocante = '';
+    this.url = '';
+    this.contacto = '';
+    this.titulo = '';
+    this.f_inicioPresentacion = new Date();
+    this.f_finPresentacion = new Date();;
+    this.plazas = null;
+
+    this.showSuccess = false;
+    this.showFFinError = false;
+    this.showFInitError = false;
+    this.showConvError = false;
+    this.showDenomError = false;
+
+  }
+
+  createOferta() {
+    //this.clean();
+
+    const ofertaData = { // Objeto usuario en registro
+      algo: this.f_finPresentacion,
+      otro: this.f_inicioPresentacion,
+    };
+
+    /*this.http.post('http://localhost:3000' + '/login', userData,{withCredentials: true} ).subscribe(
+      (resp: any) => {
+        console.log('resp');
+        console.log(resp);
+        console.log('logueado');
+        this.showSuccess = true;
+        setTimeout(() => {
+          this.router.navigate(['inicio']);
+        }, 1000);
+      },
+      (error: HttpErrorResponse) => {
+        if (error.error == "Usuario no existe") {
+          this.showNameError = true;
+        }
+        if (error.error == "Pass erronea") {
+          this.showPassError = true;
+        }
+      });*/
+
   }
 
 }
